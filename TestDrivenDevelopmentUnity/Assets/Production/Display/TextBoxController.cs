73bf2bc60afa3eba98ae5e90d9a45c1b40d74c64
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-24 19:40:21 +0000

    Cleaned Update Text code.

--- TestDrivenDevelopmentUnity/Assets/Production/Display/TextBoxController.cs
+++ TestDrivenDevelopmentUnity/Assets/Production/Display/TextBoxController.cs
@@ -16,28 +16,40 @@ namespace Production.Display
             ETextBoxInteraction interaction = ETextBoxInteraction.Automatic
             )
         {
+            UpdateDisplayText(newValue);
+            UpdateInteractionImage(interaction);
+        }
+
+        private void UpdateDisplayText(string newText)
+        {
             if (DisplayText is null)
             {
                 Debug.LogWarning($"{nameof(DisplayText)} was null. Nothing was updated.");
                 return;
             }
             
-            DisplayText.text = newValue;
+            DisplayText.text = newText;
+        }
 
-            if (InteractionImage is not null)
+        private void UpdateInteractionImage(ETextBoxInteraction interaction)
+        {
+            if (InteractionImage is null)
             {
-                Color current = InteractionImage.color;
-                if (interaction == ETextBoxInteraction.Automatic)
-                {
-                    current.a = 0;
-                }
-                else
-                {
-                    current.a = 1;
-                }
-                
-                InteractionImage.color = current;
+                Debug.LogWarning($"{nameof(InteractionImage)} was null. Nothing was updated.");
+                return;
             }
+
+            Color current = InteractionImage.color;
+            if (interaction == ETextBoxInteraction.Automatic)
+            {
+                current.a = 0;
+            }
+            else
+            {
+                current.a = 1;
+            }
+            
+            InteractionImage.color = current;
         }
     }
 }
